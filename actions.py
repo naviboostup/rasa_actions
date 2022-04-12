fdcdbb2e85c4aea10a05d2b2b0b421fc6c07d3b8
atulrajdutta <atulrajdutta@gmail.com>
2022-04-12 14:26:59 +0530

    removed agent talk

--- actions.py
+++ actions.py
@@ -123,7 +123,7 @@ class ActionSubmitSearchProgramCode(Action):
             response="utter_invalid_code"
         )
 
-        # url = "http://2.56.215.239:3010/api/student/getCutOff"
+        # url = "https://mohe.omantel.om/moheapp/api/student/getCutOff"
         # querystring = {"programCode": code_from_user}
         # payload = ""
         # response = requests.request("GET", url, data=payload, params=querystring)
@@ -180,7 +180,7 @@ class ActionSubmitProgramCutoff(Action):
 #            response="utter_invalid_code"
 #        )
 
-        url = "http://2.56.215.239:3010/api/student/getCutOff"
+        url = "https://mohe.omantel.om/moheapp/api/student/getCutOff"
         querystring = {"programCode": code_from_user}
         payload = ""
         response = requests.request("GET", url, data=payload, params=querystring)
@@ -2462,12 +2462,12 @@ class ActionDefaultFallback(Action):
 
         if number_of_fallback == 1:
             senders_maintain["sender"] = 0
-            # dispatcher.utter_message(
-            #     response="utter_default_fallback"
-            # )
+            dispatcher.utter_message(
+                response="utter_default_fallback"
+            )
 
-            # return [UserUtteranceReverted()]
-            return [AllSlotsReset(), FollowupAction('humanhandoff_yesno_form')]
+            return [UserUtteranceReverted()]
+            # return [AllSlotsReset(), FollowupAction('humanhandoff_yesno_form')]
         else:
             dispatcher.utter_message(
                 response="utter_unidentified_input"
@@ -2981,7 +2981,7 @@ class AskForOtp(Action):
             phone_number = tracker.sender_id[3:]
         main_menu_option = tracker.get_slot("main_menu")
 
-        url = "http://2.56.215.239:3010/api/student/checkAvailability"
+        url = "https://mohe.omantel.om/moheapp/api/student/checkAvailability"
 
         if tracker.get_latest_input_channel().lower() == "web":
             querystring = {"civil": civil_number, "mobileNumber": phone_number, "web": "1"}
@@ -3037,7 +3037,7 @@ class ActionSubmitOfferForm(Action):
             phone_number = tracker.sender_id[3:]
         main_menu_option = tracker.get_slot("main_menu")
 
-        url = "http://2.56.215.239:3010/api/student/checkAvailability/duplicate"
+        url = "https://mohe.omantel.om/moheapp/api/student/checkAvailability/duplicate"
 
         if tracker.get_latest_input_channel().lower() == "web":
             querystring = {"civil": civil_number, "mobileNumber": phone_number, "web": "1"}
@@ -3112,7 +3112,7 @@ class ActionSubmitOfferYesNoForm(Action):
         else:
             main_menu_option = tracker.get_slot("main_menu")
         if tracker.get_slot('offer_yesno') == '1':
-            url = "http://2.56.215.239:3010/api/student/getOffer"
+            url = "https://mohe.omantel.om/moheapp/api/student/getOffer"
             querystring = {"civil": civil_number, "type": main_menu_option}
             payload = ""
             response = requests.request("GET", url, data=payload, params=querystring)
